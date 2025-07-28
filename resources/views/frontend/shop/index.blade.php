44a3fa27440a414f0603067fc68e6505eb9609c2
Qasim1444 <qqasimjaved@gmail.com>
2025-07-28 18:40:13 +0500

    sss

--- resources/views/frontend/shop/index.blade.php
+++ resources/views/frontend/shop/index.blade.php
@@ -107,7 +107,7 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <script>
 $(document).ready(function() {
-    alert('test');
+
     $('.add-to-cart').on('click', function() {
         const productId = $(this).data('product-id');
         $.ajax({
